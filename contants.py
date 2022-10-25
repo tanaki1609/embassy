d2d282b08dd4b6583fb4d06e6e67bdde1e638475
Anonimous05 <airastolonov05@gmail.com>
2022-10-25 17:16:44 +0600

    small corrections

--- contants.py
+++ contants.py
@@ -12,3 +12,26 @@ MONTHS_BY_POSITION = {
     11: 'Ноябрь',
     12: 'Декабрь'
 }
+
+HEADERS = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7',
+    'Connection': 'keep-alive',
+    'Content-Length': '52',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Cookie': 'TMOSHCooKie=S+E+cJwAhsS+9bKnB81hijAJAf0bFveQ0m4fDjXUHfShZAouy64POcCbmoXBDTlz2/9E3sjUgQCOoJeuSnUi1LL7O09ib7sGBPEAAAAB; SCOUTER=z2tbdo7svf2fqv; clientid=010031612824; TMOSHCooKie=UfDLRZwsU2YtOxSnB81hijAJAf0bFhs06pOF1fUJhHl+auBsXISMJ/LRpNTWB7YvFA9MiPXrh8itF3WW6nWYMq8dB7k5bjkcYBsAAAAB; JSESSIONID=tcrBplUgImclXsIDYq+5-Yev.cip22',
+    'Host': 'consul.mofa.go.kr',
+    'Origin': 'https://consul.mofa.go.kr',
+    'Referer': 'https://consul.mofa.go.kr/ciph/0800/selectCIPH0801Deng.do',
+    'sec-ch-ua': '\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '\"Linux\"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+BASE_URL = 'https://consul.mofa.go.kr'
--- main.py
+++ main.py
@@ -3,44 +3,25 @@ import time
 import datetime
 import requests
 from dateutil.relativedelta import relativedelta
-from contants import MONTHS_BY_POSITION
+from contants import MONTHS_BY_POSITION, HEADERS, BASE_URL
 
 
 def check_date(date) -> bool:
     try:
-        r = requests.post(url='https://consul.mofa.go.kr/ciph/0800/selectVisitReserveCalendarYes.do',
+        r = requests.post(url=f'{BASE_URL}/ciph/0800/selectVisitReserveCalendarYes.do',
                           data={
                               'emblCd': 'KY',
                               'visitResveBussGrpCd': 'KY0001',
                               'emblTime': f'{date.year}{date.month}'
                           },
-                          headers={
-                              'Accept': 'application/json, text/javascript, */*; q=0.01',
-                              'Accept-Encoding': 'gzip, deflate, br',
-                              'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7',
-                              'Connection': 'keep-alive',
-                              'Content-Length': '52',
-                              'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                              'Cookie': 'TMOSHCooKie=S+E+cJwAhsS+9bKnB81hijAJAf0bFveQ0m4fDjXUHfShZAouy64POcCbmoXBDTlz2/9E3sjUgQCOoJeuSnUi1LL7O09ib7sGBPEAAAAB; SCOUTER=z2tbdo7svf2fqv; clientid=010031612824; TMOSHCooKie=UfDLRZwsU2YtOxSnB81hijAJAf0bFhs06pOF1fUJhHl+auBsXISMJ/LRpNTWB7YvFA9MiPXrh8itF3WW6nWYMq8dB7k5bjkcYBsAAAAB; JSESSIONID=tcrBplUgImclXsIDYq+5-Yev.cip22',
-                              'Host': 'consul.mofa.go.kr',
-                              'Origin': 'https://consul.mofa.go.kr',
-                              'Referer': 'https://consul.mofa.go.kr/ciph/0800/selectCIPH0801Deng.do',
-                              'sec-ch-ua': '\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"',
-                              'sec-ch-ua-mobile': '?0',
-                              'sec-ch-ua-platform': '\"Linux\"',
-                              'Sec-Fetch-Dest': 'empty',
-                              'Sec-Fetch-Mode': 'cors',
-                              'Sec-Fetch-Site': 'same-origin',
-                              'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-                              'X-Requested-With': 'XMLHttpRequest'
-                          })
+                          headers=HEADERS)
         response = json.loads(r.content)
         data = [date['visitYn'].lower() for date in response['visitReserveCalendarYesResult']]
 
         if response and 'y' in data:
             message = f'Есть место {MONTHS_BY_POSITION.get(date.month)}!'
             requests.post(
-                f'https://api.telegram.org/bot5430523711:AAHFQFT5_dfsZO04cmjQfQGHwMJXm3-YNoI/sendMessage?chat_id=353761483&text={message}'
+                f'https://api.telegram.org/bot5430523711:AAHFQFT5_dfsZO04cmjQfQGHwMJXm3-YNoI/sendMessage?chat_id=5472619858&text={message}'
             )
             return True
     except:
@@ -53,8 +34,8 @@ if __name__ == '__main__':
     next_ = datetime.datetime.today() + relativedelta(months=1)
 
     a = time.perf_counter()
-    check_date(now)
-    check_date(next_)
+    have_now = check_date(now)
+    have_next = check_date(next_)
     b = time.perf_counter()
 
     current_time = now.strftime("%H:%M:%S")
